b6979f736cb3649c27e2d7c77b9090175e2995ca
doodspav <doods9doods@gmail.com>
2018-05-29 19:40:33 +0100

    Update loadTables.py

--- Scripts/loadTables.py
+++ Scripts/loadTables.py
@@ -5,7 +5,7 @@ database = "<put db name here>"
 files = [f for f in glob.glob("*.sql")]
 for f in files:
     try:
-        command = "mysql `%s` < %s" % (database,f)
+        command = "mysql %s < %s" % (database,f)
         stdout = subprocess.check_output(command,shell=True)
     except Exception:
         print("Database (%s) not found." % database)
